6af1f2e36e41d316aacdf186fa5731235e8faec4
Pragati Veer <veer.pragati99@gmail.com>
2024-12-30 02:44:50 +0530

    Update code with Firebase deployment

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Checkout from "./Checkout";
 import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from "./Payment";
 function App() {
   const [{}, dispatch] = useStateValue();
   useEffect(() => {
@@ -30,12 +31,22 @@ function App() {
       <div className="app">
         <Routes>
           <Route path="/login" element={<Login />} />
+
           <Route
             path="/checkout"
             element={
               <>
                 <Header />
                 <Checkout />
+              </>
+            }
+          />
+          <Route
+            path="/payment"
+            element={
+              <>
+                <Header />
+                <Payment />
               </>
             }
           />
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Product from "./Product";
 import "./Home.css";
+
 function Home() {
   return (
     <div className="home">
@@ -10,6 +11,8 @@ function Home() {
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt=""
         />
+        {/* Using FlipMove for smooth animations */}
+
         <div className="home_row">
           <Product
             id="12321341"
