b60732563443ffec7837c424bd17b7ee13559476
Pragati Veer <veer.pragati99@gmail.com>
2024-12-29 23:17:37 +0530

    Update code with all screen

--- src/Header.js
+++ src/Header.js
@@ -2,23 +2,42 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const handleAuthenticaton = () => {
+    if (user) {
+      console.log("user logoutr");
+      auth.signOut();
+    }
+  };
   return (
     <div className="header">
-      <img
-        className="header_logo"
-        alt="logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      />
+      <Link to="/">
+        <img
+          className="header_logo"
+          alt="logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
+
       <div className="header_search">
         <input className="header_searchInput" type="text" />
         <SearchIcon className="header_searchIcon" />
       </div>
       <div className="header_nav">
-        <div className="header_option">
-          <span className="header_optionLineOne">Hello guest</span>
-          <span className="header_optionLineTwo">Sing in</span>
-        </div>
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthenticaton} className="header_option">
+            <span className="header_optionLineOne">
+              Hello {!user ? "Guest" : user.email}
+            </span>
+            <span className="header_optionLineTwo">
+              {user ? "Sign Out" : "Sign In"}
+            </span>
+          </div>
+        </Link>
         <div className="header_option">
           <span className="header_optionLineOne">Returns</span>
           <span className="header_optionLineTwo">& Orders</span>
@@ -27,10 +46,14 @@ function Header() {
           <span className="header_optionLineOne">Your</span>
           <span className="header_optionLineTwo">Prime</span>
         </div>
-        <div className="header_optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header_optionLineTwo_basketCount">0</span>
-        </div>
+        <Link to="/checkout">
+          <div className="header_optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header_optionLineTwo_basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
       </div>
     </div>
   );
